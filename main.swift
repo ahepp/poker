5c0e026b852bbc1941cf8df1bc7974e6b125385e
Andrew Hepp <andrew.hepp@ahepp.dev>
2022-04-13 13:12:03 -0700

    Change the temperature of my ember mug to 135f. Not generalized yet.

--- main.swift
+++ main.swift
@@ -1,23 +1,28 @@
 import CoreBluetooth
 import Foundation
 
-class EmberDelegate: NSObject, CBCentralManagerDelegate {
-  private var mgr: CBCentralManager!
+class EmberDelegate: NSObject, CBCentralManagerDelegate, CBPeripheralDelegate {
+  private var active = true;
+  private var emberServiceId = CBUUID(string: "FC543622-236C-4C94-8FA9-944A3E5353FA")
+  private var emberPeripheralId = UUID(uuidString: "3217D429-F44C-EF48-B66E-ADFBF44ECDAC")
+  private var emberCharacteristicId = CBUUID(string: "FC540003-236C-4C94-8FA9-944A3E5353FA")
+  private var mug: CBPeripheral!
 
-  override init() {
-    print("init EmberDelegate")
-    super.init()
-    mgr = CBCentralManager(delegate: self, queue: nil)
+  func isActive() -> Bool {
+      return active;
   }
-
+  
   func centralManagerDidUpdateState(_ central: CBCentralManager) {
-    print("didUpdateState")
     switch central.state {
     case .poweredOn:
-      print("on")
+      // TODO why doesn't this work? UUID shows up in services later...
+      //central.scanForPeripherals(withServices: [emberServiceId])
       central.scanForPeripherals(withServices: nil)
+      print("started scan")
     default:
-      print(central.state.rawValue)
+      print("centralManagerDidUpdateState: " + String(central.state.rawValue))
+      central.stopScan()
+      print("stopped scan")
     //case .poweredOff:
     //case .resetting:
     //case .unauthorized:
@@ -27,23 +32,48 @@ class EmberDelegate: NSObject, CBCentralManagerDelegate {
   }
 
   func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-    print("didDiscover")
+      guard peripheral.identifier == emberPeripheralId else {return}
+      print("discovered " + peripheral.identifier.uuidString)
+
+      central.stopScan()
+      print("stopped scan")
+      central.connect(peripheral, options: nil)
+      mug = peripheral
   }
   
   func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
-    print("didConnect")
+    print("connected to " + peripheral.identifier.uuidString)
+    peripheral.delegate = self
     peripheral.discoverServices(nil)
   }
   
-  func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
-    print("didDisconnect")
-    central.scanForPeripherals(withServices: nil, options: nil)
+  func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+    if let ss = peripheral.services {
+      for s in ss {
+        print("discovered service " + s.uuid.uuidString)
+        peripheral.discoverCharacteristics(nil, for: s)
+      }
+    }
+  }
+
+  func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+    if let cs = service.characteristics {
+      for c in cs {
+        print("discovered service: " + service.uuid.uuidString + " characteristic " + c.uuid.uuidString)
+        if c.uuid == emberCharacteristicId {
+          let data = Data(_ : [0x59, 0x16])
+          peripheral.writeValue(data, for: c, type: .withResponse)
+          active = false
+        }
+      }
+    }
   }
 } // EmberDelegate
 
-print("Hello, world!")
-let ember = EmberDelegate()
-while true {
+let emberDelegate = EmberDelegate()
+let queue = DispatchQueue(label: "ConcurrentQueue", qos: .default, attributes: .concurrent)
+let mgr = CBCentralManager(delegate: emberDelegate, queue: queue)
+while emberDelegate.isActive() {
   let US_TO_S: useconds_t = 1000000
   usleep(1 * US_TO_S)
 }
